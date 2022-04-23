00de351c65cee2a56d6fb0080f103211999a59a8
arishazz <alamarisha18@gmail.com>
2022-04-22 20:54:25 -0400

    added into pycharm

--- hangman.py
+++ hangman.py
@@ -1,15 +1,11 @@
-
 import random
-
 
 print("Welcome to HangMan!")
 Continue = input("q to quit,r for rules, else to start/continue!")
 
 wordsList = []
 
-
-    
-with open('words.txt','r') as f:
+with open('words.txt', 'r') as f:
     for line in f:
         for word in line.split():
             wordsList.append(word)
@@ -20,7 +16,6 @@ print(chosenWord)
 wordLength = len(chosenWord)
 dashesExtra = wordLength - 4
 dashes = ["_", "_", "_", "_"]
-    
 
 for i in range(dashesExtra):
     dashes.append("_")
@@ -28,31 +23,28 @@ print(' '.join(dashes))
 
 
 def guessing():
-    letter = input("Guess a letter!")
+    hearts = 6
+    print(hearts, "guesses left!")
+    letter = input("Guess!")
+    count = chosenWord.count(letter)
+    print(count)
+
     if letter not in chosenWord:
-         print("""
-         +---+
-         |   |
-         O   |
-             |
-             |
-             |
-     =========
-     """)
+        hearts = hearts - 1
+        print(hearts, "guesses left!")
+    elif hearts == 0:
+        print("you lose!")
+        return
 
-
+    elif letter in chosenWord and count > 1:
+        print("working")
+        return
     else:
-        # position = (chosenWord.find(letter))
-        # dashes[position] = letter
-
-        remaining = chosenWord.count(letter)
-        position = 0
-        while (remaining!=0):
-            position = chosenWord.find(letter, position)
-            dashes[position]= letter
-            remaining -= 1
-        
+        position = (chosenWord.find(letter))
+        dashes[position] = letter
         print(' '.join(dashes))
+
+
 if Continue == "r":
     print("1.) Don't add a space before your guess")
     print("2.) Guess must be in lowercase")
@@ -60,7 +52,6 @@ if Continue == "r":
     print("4.) We don't allow word guesses D:")
 else:
     guessing()
-    
 
 '''
 chosenWord = random.choice(wordsList)
@@ -76,23 +67,7 @@ for i in range(dashesExtra):
 print(' '.join(dashes))
 '''
 
-        
-    
-      
-
 while Continue != "q":
-    
     guessing()
-        
-   
-        
-
 
 f.close()
-
-
-
-
-
-
-
